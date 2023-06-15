ffb06ae7f17d266ffb738a3a7105984218bd1a72
appishiwata <app.ishiwata@gmail.com>
2023-06-16 06:40:18 +0900

    UniTask.WhenAllを使ってみた

--- Assets/Scenes/Async/AsyncSample.cs
+++ Assets/Scenes/Async/AsyncSample.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class AsyncSample : MonoBehaviour
 {
     [SerializeField] Button _closeButton;
+    [SerializeField] Button _openButton;
 
     async void Start()
     {
@@ -14,6 +15,32 @@ public class AsyncSample : MonoBehaviour
 
         //ボタンが押されるまで待つ
         await _closeButton.OnClickAsync();
-        Debug.Log("Hello");
+        Debug.Log("close");
+
+        //ボタンが押されるまで待つ
+        await _openButton.OnClickAsync();
+        Debug.Log("open");
+
+        //すべての処理が終わるまで待つ
+        await UniTask.WhenAll(
+            _closeButton.OnClickAsync(),
+            _openButton.OnClickAsync()
+        );
+        Debug.Log("close and open");
+
+        //指定した秒数待機する3,4,5秒
+        await UniTask.WhenAll(
+            WaitAsync(3f),
+            WaitAsync(4f),
+            WaitAsync(5f)
+        );
+        Debug.Log("3,4,5 seconds wait");
+    }
+
+    //指定した秒数待機する
+    async UniTask WaitAsync(float seconds)
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(seconds));
+        Debug.Log($"Wait {seconds} seconds");
     }
 }
