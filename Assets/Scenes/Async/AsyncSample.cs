e698cc81576d827d2de7333883e9d8fef7f5f573
appishiwata <app.ishiwata@gmail.com>
2023-06-16 05:56:33 +0900

    ボタンが押されるまで待つように

--- Assets/Scenes/Async/AsyncSample.cs
+++ Assets/Scenes/Async/AsyncSample.cs
@@ -1,12 +1,19 @@
 using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AsyncSample : MonoBehaviour
 {
+    [SerializeField] Button _closeButton;
+
     async void Start()
     {
-        await UniTask.Delay(TimeSpan.FromSeconds(3f));
+        //3秒待つ
+        //await UniTask.Delay(TimeSpan.FromSeconds(3f));
+
+        //ボタンが押されるまで待つ
+        await _closeButton.OnClickAsync();
         Debug.Log("Hello");
     }
 }
