cf06be7ba335f88dc7df4704b9d40d98cf8d294d
appishiwata <app.ishiwata@gmail.com>
2023-06-17 09:00:29 +0900

    ボタンを押したらTitleオブジェクトを破棄するように canceltokenありの場合確認

--- Assets/Scenes/Async/Title.cs
+++ Assets/Scenes/Async/Title.cs
@@ -12,18 +12,48 @@ public class Title : MonoBehaviour
         text.text = "Title";
         Debug.Log("Title");
 
+        //以下全ての待機処理をcancelTokenを使ってキャンセルできるようにする
+        var cancelToken = this.GetCancellationTokenOnDestroy();
+
         //2秒待ってからテキストを変更する
-        await UniTask.Delay(TimeSpan.FromSeconds(2f));
+        //try-catchでUniTask.Delayの例外をキャッチする
+        try
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(2f), cancellationToken: cancelToken);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return;
+        }
         text.text = "Start";
         Debug.Log("Start");
 
         //2秒待ってからテキストの色を変更する
-        await UniTask.Delay(TimeSpan.FromSeconds(2f));
+        //try-catchでUniTask.Delayの例外をキャッチする
+        try
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(2f), cancellationToken: cancelToken);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return;
+        }
         text.color = Color.red;
         Debug.Log("Color.red");
 
         //5秒待ってからテキストのサイズを変更する
-        await UniTask.Delay(TimeSpan.FromSeconds(5f));
+        //try-catchでUniTask.Delayの例外をキャッチする
+        try
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(5f), cancellationToken: cancelToken);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return;
+        } 
         text.fontSize = 100;
         Debug.Log("fontSize 100");
     }
