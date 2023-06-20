b976ae7048bac9027939946360ab9a03db524eb7
appishiwata <app.ishiwata@gmail.com>
2023-06-21 08:41:21 +0900

    色変更、移動、回転、シェイクなどのアニメーションを追加

--- Assets/Scenes/Anim/AnimSample.cs
+++ Assets/Scenes/Anim/AnimSample.cs
@@ -6,13 +6,47 @@ using UnityEngine;
 
 public class AnimSample : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI _colorText;
+    [SerializeField] TextMeshProUGUI _moveText;
+    [SerializeField] TextMeshProUGUI _rotateText;
+    [SerializeField] TextMeshProUGUI _shakeText;
     [SerializeField] TextMeshProUGUI _sampleText;
 
     void Start()
     {
+        // テキストを設定
+        _colorText.text = "Color";
+        _moveText.text = "Move";
+        _rotateText.text = "Rotate";
+        _shakeText.text = "Shake";
         _sampleText.text = "Sample";
 
-        // dotweenでアニメーションを作成
-        DOTween.To(() => _sampleText.color, x => _sampleText.color = x, Color.red, 1f);
+        // _colorTextの色をdotweenでアニメーション
+        DOTween.To(() => _colorText.color, x => _colorText.color = x, Color.red, 1f).SetEase(Ease.OutBounce);
+        // _colorTextの色をdotweenでアニメーション 2秒後に実行
+        DOTween.To(() => _colorText.color, x => _colorText.color = x, Color.blue, 1f).SetEase(Ease.OutBounce).SetDelay(2f);
+
+        // _moveTextの移動をdotweenでアニメーション
+        _moveText.transform.DOMoveX(100f, 1f).SetRelative();
+        // _moveTextの移動をdotweenでアニメーション 2秒後に実行
+        _moveText.transform.DOMoveX(-100f, 1f).SetRelative().SetDelay(2f);
+
+        // _rotateTextの回転をdotweenでアニメーション
+        _rotateText.transform.DORotate(new Vector3(0f, 0f, 360f), 1f, RotateMode.FastBeyond360).SetRelative();
+        // _rotateTextの回転をdotweenでアニメーション 2秒後に実行
+        _rotateText.transform.DORotate(new Vector3(0f, 0f, -360f), 1f, RotateMode.FastBeyond360).SetRelative().SetDelay(2f);
+
+        // _shakeTextのシェイクをdotweenでアニメーション
+        _shakeText.transform.DOShakePosition(1f, 10f, 10, 90f, false, true);
+        // _shakeTextのシェイクをdotweenでアニメーション 2秒後に実行
+        _shakeText.transform.DOShakePosition(1f, 10f, 10, 90f, false, true).SetDelay(2f);
+
+        // _sampleTextのアニメーションをdotweenで作成 完了後 徐々に非表示にする
+        DOTween.Sequence()
+            .AppendInterval(2f)
+            .Append(_sampleText.transform.DOScale(1.5f, 0.5f))
+            .Append(_sampleText.transform.DOScale(1f, 0.5f))
+            .SetLoops(2)
+            .OnComplete(() => _sampleText.DOFade(0f, 1f).SetDelay(2f));
     }
 }
