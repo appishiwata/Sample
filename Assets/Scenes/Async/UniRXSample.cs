4d34205536357effd93dfbf549d03e8d8eeb194a
appishiwata <app.ishiwata@gmail.com>
2023-06-23 04:53:24 +0900

    UniRXの導入

---
+++ Assets/Scenes/Async/UniRXSample.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class UniRXSample : MonoBehaviour
+{
+    void Start()
+    {
+        // UniRXのサンプル
+        // 1秒後に実行
+        Observable.Timer(TimeSpan.FromSeconds(1f))
+            .Subscribe(_ =>
+            {
+                Debug.Log("1秒後に実行");
+            });
+    }
+
+}
