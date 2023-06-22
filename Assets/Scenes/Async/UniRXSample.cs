8d8f431f2cf01e548d551fcabfb902e1ee8b43f9
appishiwata <app.ishiwata@gmail.com>
2023-06-23 05:06:56 +0900

    ボタンのイベント登録、連打防止も

--- Assets/Scenes/Async/UniRXSample.cs
+++ Assets/Scenes/Async/UniRXSample.cs
@@ -3,9 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UniRXSample : MonoBehaviour
 {
+    [SerializeField] Button _button;
+    [SerializeField] Button _strikeButton;
+
     void Start()
     {
         // UniRXのサンプル
@@ -15,6 +19,20 @@ public class UniRXSample : MonoBehaviour
             {
                 Debug.Log("1秒後に実行");
             });
-    }
 
+        // _buttonのクリックイベントを登録
+        _button.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                Debug.Log("ボタンがクリックされた");
+            });
+
+        // _strikeButtonのクリックイベントを登録 連打防止
+        _strikeButton.OnClickAsObservable()
+            .ThrottleFirst(TimeSpan.FromSeconds(1f))
+            .Subscribe(_ =>
+            {
+                Debug.Log("ボタンがクリックされた");
+            });
+    }
 }
