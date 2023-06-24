e39b6d10fa8de9c65f050d6f0fca50da5af1b0c8
appishiwata <app.ishiwata@gmail.com>
2023-06-24 13:38:19 +0900

    List/Array/Dictionary各種形式のサンプル

--- Assets/Scenes/Linq/LinqSample.cs
+++ Assets/Scenes/Linq/LinqSample.cs
@@ -7,6 +7,64 @@ public class LinqSample : MonoBehaviour
 {
     void Start()
     {
+        //GetLinqSample();
+        //GetListSample();
+        //GetArraySample();
+        //GetDictionarySample();
+    }
+
+    void GetDictionarySample()
+    {
+        // Dictionaryのサンプル
+        var dictionary = new Dictionary<int, string>
+        {
+            { 1, "aa" },
+            { 2, "bb" },
+            { 3, "cc" },
+            { 4, "dd" },
+            { 5, "ee" },
+        };
+        // Dictionaryの要素を変更
+        dictionary = dictionary.ToDictionary(x => x.Key, x => x.Value + x.Value);
+        // Dictionaryのメソッドサンプル
+        dictionary.Add(6, "6");
+        dictionary.Remove(1);
+        dictionary.Clear();
+    }
+
+    void GetArraySample()
+    {
+        // Arrayのサンプル
+        var array = new[] { 1, 2, 3, 4, 5 };
+        // Arrayの要素をすべて2倍にする
+        array = array.Select(i => i * 2).ToArray();
+        // Arrayのメソッドサンプル
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        array = array.Where(i => i % 2 == 0).ToArray();
+    }
+
+    void GetListSample()
+    {
+        // Listのサンプル
+        var list = new List<int> { 1, 2, 3, 4, 5 };
+        // Listの要素をすべて2倍にする
+        list = list.Select(i => i * 2).ToList();
+        // Listのメソッドサンプル
+        list.Add(6);
+        list.Remove(1);
+        list.RemoveAt(0);
+        list.Insert(0, 1);
+        list.Sort();
+        list.Reverse();
+        list.Clear();
+    }
+
+    void GetLinqSample()
+    {
         // Linqのサンプル
         var list = new List<int> { 1, 2, 3, 4, 5 };
         var result = list.Where(i => i % 2 == 0);
