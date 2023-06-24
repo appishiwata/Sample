1a8ae004b1d388a5b515bf8a187b7fc454a70a0f
appishiwata <app.ishiwata@gmail.com>
2023-06-24 13:00:03 +0900

    Selectの実行, List/Array/Dictionaryに変換

--- Assets/Scenes/Linq/LinqSample.cs
+++ Assets/Scenes/Linq/LinqSample.cs
@@ -12,7 +12,39 @@ public class LinqSample : MonoBehaviour
         var result = list.Where(i => i % 2 == 0);
         foreach (var i in result)
         {
-            Debug.Log(i);
+            Debug.Log("result: " + i);
+        }
+
+        // Linqのサンプル
+        var list2 = new List<int> { 1, 2, 3, 4, 5 };
+        var result2 = list2.Where(i => i % 2 == 0).Select(i => i * 2);
+        foreach (var i in result2)
+        {
+            Debug.Log("result2: " + i);
+        }
+
+        // Linqのサンプル
+        var list3 = new List<int> { 1, 2, 3, 4, 5 };
+        var result3 = list3.Where(i => i % 2 == 0).Select(i => i * 2).ToList();
+        foreach (var i in result3)
+        {
+            Debug.Log("result3: " + i);
+        }
+
+        // Linqのサンプル
+        var list4 = new List<int> { 1, 2, 3, 4, 5 };
+        var result4 = list4.Where(i => i % 2 == 0).Select(i => i * 2).ToArray();
+        foreach (var i in result4)
+        {
+            Debug.Log("result4: " + i);
+        }
+
+        // Linqのサンプル
+        var list5 = new List<int> { 1, 2, 3, 4, 5 };
+        var result5 = list5.Where(i => i % 2 == 0).Select(i => i * 2).ToDictionary(i => i);
+        foreach (var i in result5)
+        {
+            Debug.Log("result5: " + i);
         }
     }
 }
