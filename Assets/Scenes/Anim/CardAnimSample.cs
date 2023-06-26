04fb3860fd8c222db9672af1188edf05b22e6631
appishiwata <app.ishiwata@gmail.com>
2023-06-27 01:31:42 +0900

    カードが切れて上下に移動するアニメーション

--- Assets/Scenes/Anim/CardAnimSample.cs
+++ Assets/Scenes/Anim/CardAnimSample.cs
@@ -8,20 +8,27 @@ public class CardAnimSample : MonoBehaviour
 {
 
     [SerializeField] Image _cardImage;
+    [SerializeField] Image _cardImageUpper;
+    [SerializeField] Image _cardImageLower;
+    [SerializeField] Image _characterImage;
 
     void Start()
     {
+        // _cardImageをdotweenで上に移動させる 終わったら左上に移動させる
         _cardImage.transform.DOMoveY(400f, 2f)
             .SetRelative()
             .SetEase(Ease.OutBounce)
-            .SetDelay(0.5f)
             .OnComplete(() =>
             {
-                DOTween.Sequence()
-                    .AppendInterval(1f)
-                    .Append(_cardImage.transform.DOScale(1.1f, 1f))
-                    .Append(_cardImage.transform.DOScale(1f, 1f))
-                    .SetLoops(-1);
+                //_cardImageのalphaを0にする
+                _cardImage.DOFade(0f, 0.1f);
+                // _cardImageUpperと_cardImageLowerを上下にアニメーションさせる
+                _cardImageUpper.transform.DOMove(new Vector3(-100f, 800f, 0f), 1.5f)
+                    .SetDelay(0.5f)
+                    .SetRelative();
+                _cardImageLower.transform.DOMove(new Vector3(100f, -800f, 0f), 1.5f)
+                    .SetDelay(0.5f)
+                    .SetRelative();
             });
     }
 }
