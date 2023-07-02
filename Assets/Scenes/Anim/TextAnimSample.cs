7454960716b956ab9a729b30b9a94b8b604f168c
appishiwata <app.ishiwata@gmail.com>
2023-07-02 23:59:47 +0900

    FillAmount

--- Assets/Scenes/Anim/TextAnimSample.cs
+++ Assets/Scenes/Anim/TextAnimSample.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextAnimSample : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _titleText;
     [SerializeField] TextMeshProUGUI _countText;
+    [SerializeField] Image _image;
 
     void Start()
     {
@@ -18,5 +20,8 @@ public class TextAnimSample : MonoBehaviour
 
         // _countTextをDOCounterでアニメーション
         _countText.DOCounter(0, 100, 2.0f).SetEase(Ease.Linear);
+
+        // _imageをDOFillAmountでアニメーション
+        _image.DOFillAmount(1.0f, 2.0f).SetEase(Ease.Linear);
     }
 }
