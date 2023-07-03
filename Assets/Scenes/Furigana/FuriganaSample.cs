68701ba82d2af1c0b2d897a789cbc03b7cd758e0
appishiwata <app.ishiwata@gmail.com>
2023-07-03 11:28:07 +0900

    ふりがなを動的に

--- Assets/Scenes/Furigana/FuriganaSample.cs
+++ Assets/Scenes/Furigana/FuriganaSample.cs
@@ -9,6 +9,35 @@ public class FuriganaSample : MonoBehaviour
 
     void Start()
     {
-        _titleText.text = "発電所<style=p6>はつでんしょ</style>";
+        // NameAndFuriganaクラス3人分のリストを作成
+        var nameAndFuriganaList = new List<NameAndFurigana>()
+        {
+            new NameAndFurigana("発電所", "はつでんしょ"),
+            new NameAndFurigana("風", "かぜ"),
+            new NameAndFurigana("回復", "かいふく"),
+        };
+
+        // リストの要素をランダムに取得
+        var nameAndFurigana = nameAndFuriganaList[Random.Range(0, nameAndFuriganaList.Count)];
+
+        // furiganaの文字数を取得
+        int furiganaLength = nameAndFurigana.Furigana.Length;
+
+        // NameとFuriganaをTextMeshProUGUIに設定
+        _titleText.text = $"{nameAndFurigana.Name}<style=p{furiganaLength}>{nameAndFurigana.Furigana}</style>";
+    }
+}
+
+
+// NameとFuriganaを持つクラス
+public class NameAndFurigana
+{
+    public string Name { get; set; }
+    public string Furigana { get; set; }
+
+    public NameAndFurigana(string name, string furigana)
+    {
+        Name = name;
+        Furigana = furigana;
     }
 }
