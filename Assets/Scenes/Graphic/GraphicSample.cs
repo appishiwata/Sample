09df434be8297a2f71617c8f86830fb4729db668
appishiwata <app.ishiwata@gmail.com>
2023-07-04 23:50:51 +0900

    ボタン押下でマテリアル変更

--- Assets/Scenes/Graphic/GraphicSample.cs
+++ Assets/Scenes/Graphic/GraphicSample.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GraphicSample : MonoBehaviour
 {
     [SerializeField] GameObject _sphereObject;
-    [SerializeField] Material _sphereMaterial;
+    [SerializeField] Material _redMaterial;
+    [SerializeField] Material _blueMaterial;
+    [SerializeField] Button _button;
 
     void Start()
     {
-        _sphereObject.GetComponent<Renderer>().material = _sphereMaterial;
+        _sphereObject.GetComponent<Renderer>().material = _redMaterial;
+
+        // _buttonを押したら_sphereObjectのマテリアルを_blueMaterialに変更する
+        _button.onClick.AddListener(() =>
+        {
+            _sphereObject.GetComponent<Renderer>().material = _blueMaterial;
+        });
     }
 }
