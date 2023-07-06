2c50766eb141af30a6d83771be250f073748fd28
appishiwata <app.ishiwata@gmail.com>
2023-07-07 00:18:20 +0900

    Linqを使って絞り込み 子オブジェクトの位置に移動

--- Assets/Scenes/Position/PositionSample.cs
+++ Assets/Scenes/Position/PositionSample.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PositionSample : MonoBehaviour
@@ -14,11 +15,12 @@ public class PositionSample : MonoBehaviour
         //_sphere.transform.position = new Vector3(0, 0, 0);
         //_sphere.transform.position = _target.transform.position;
         //_parentの子オブジェクトを取得
-        var children = _parent.GetComponentsInChildren<Transform>();
+        var children = _parent.GetComponentsInChildren<Transform>().Skip(1); // 親を含まないように
         foreach (var child in children)
         {
-            Debug.Log(child.name);
             _sphere.transform.position = child.position;
         }
+        var childFirst = _parent.GetComponentsInChildren<Transform>().Where(x => x.name.StartsWith("Cube")).FirstOrDefault(); // 1個目の子
+        _sphere.transform.position = childFirst.position;
     }
 }
