72ad8494d79c95a2cded8f7265cd8980c08fa05d
appishiwata <app.ishiwata@gmail.com>
2023-07-06 23:51:53 +0900

    別のオブジェクトの位置に移動

--- Assets/Scenes/Position/PositionSample.cs
+++ Assets/Scenes/Position/PositionSample.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PositionSample : MonoBehaviour
 {
     [SerializeField] GameObject _sphere;
+    [SerializeField] GameObject _target;
 
     void Start()
     {
-        _sphere.transform.position = new Vector3(0, 0, 0);
+        //_sphere.transform.position = new Vector3(0, 0, 0);
+        _sphere.transform.position = _target.transform.position;
     }
 }
