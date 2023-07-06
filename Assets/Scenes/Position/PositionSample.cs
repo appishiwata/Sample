69316b86a672f20bc21567efd86074db7a63524a
appishiwata <app.ishiwata@gmail.com>
2023-07-06 23:58:55 +0900

    親子関係のオブジェクトの子の位置に移動

--- Assets/Scenes/Position/PositionSample.cs
+++ Assets/Scenes/Position/PositionSample.cs
@@ -6,10 +6,19 @@ public class PositionSample : MonoBehaviour
 {
     [SerializeField] GameObject _sphere;
     [SerializeField] GameObject _target;
+    [SerializeField] GameObject _parent;
+
 
     void Start()
     {
         //_sphere.transform.position = new Vector3(0, 0, 0);
-        _sphere.transform.position = _target.transform.position;
+        //_sphere.transform.position = _target.transform.position;
+        //_parentの子オブジェクトを取得
+        var children = _parent.GetComponentsInChildren<Transform>();
+        foreach (var child in children)
+        {
+            Debug.Log(child.name);
+            _sphere.transform.position = child.position;
+        }
     }
 }
