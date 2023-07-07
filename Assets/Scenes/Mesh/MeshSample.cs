75dc2d85b09c511392063028f7be7c9907690826
appishiwata <app.ishiwata@gmail.com>
2023-07-07 22:14:20 +0900

    Meshの確認用シーン追加

---
+++ Assets/Scenes/Mesh/MeshSample.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSample : MonoBehaviour
+{
+    [SerializeField] GameObject _sphere;
+
+    void Start()
+    {
+        // _sphereのMeshを取得
+        var mesh = _sphere.GetComponent<MeshFilter>().mesh;
+        // _sphereのMeshの頂点を取得
+        var vertices = mesh.vertices;
+        // _sphereのMeshの頂点を変更
+        vertices[0] = new Vector3(1, 0, 0);
+        // _sphereのMeshの頂点を設定
+        mesh.vertices = vertices;
+        // _sphereのMeshの頂点を反映
+        mesh.RecalculateBounds();
+    }
+}
