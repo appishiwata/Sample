7ea5c08d667c5f14714c4c4cc170f4c0036c4d49
appishiwata <app.ishiwata@gmail.com>
2023-07-08 12:42:10 +0900

    三角形のメッシュを作成

--- Assets/Scenes/Mesh/MeshSample.cs
+++ Assets/Scenes/Mesh/MeshSample.cs
@@ -8,6 +8,8 @@ public class MeshSample : MonoBehaviour
 
     [SerializeField] Mesh _mesh;
 
+    [SerializeField] GameObject _sampleObject;
+
     void Start()
     {
         // _sphereのMeshを取得
@@ -21,11 +23,34 @@ public class MeshSample : MonoBehaviour
         // _sphereのMeshの頂点を反映
         mesh.RecalculateBounds();
 
-
         // submeshを取得
         var subMesh = mesh.subMeshCount;
        
         // メッシュをオブジェクトに設定
         _sphere.GetComponent<MeshFilter>().mesh = _mesh;
+
+        createMesh();
+    }
+
+    void createMesh()
+    {
+        // 頂点座標の設定
+        Vector3[] vertices = new Vector3[3]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(0, 1, 0)
+        };
+
+        // 三角形のインデックス
+        int[] triangles = new int[3] { 0, 2, 1 };
+
+        // メッシュオブジェクトの作成
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        // ゲームオブジェクトの作成
+        _sampleObject.GetComponent<MeshFilter>().mesh = mesh;
     }
 }
