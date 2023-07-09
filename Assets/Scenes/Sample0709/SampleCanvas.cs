700b5116de8c68e966a9f6642b8a31c6153a1fd6
appishiwata <app.ishiwata@gmail.com>
2023-07-09 17:22:33 +0900

    dotween追加

--- Assets/Scenes/Sample0709/SampleCanvas.cs
+++ Assets/Scenes/Sample0709/SampleCanvas.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -6,11 +7,21 @@ public class SampleCanvas : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _titleText;
 
-    async void Start()
+    void Start()
     {   
+        _titleText.text = "default";
+        Debug.Log("Start");
+        
+        TextAnimation();
+    }
+
+    async void TextAnimation()
+    {
         _titleText.text = "Hello World";
-        // UniTaskで１秒待つ
         await UniTask.Delay(1000);
         _titleText.text = "Hello World2";
+        await UniTask.Delay(1000);
+        _titleText.text = "";
+        await _titleText.DOText("Hello World3", 1.0f).AsyncWaitForCompletion();
     }
 }
