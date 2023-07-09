1e76cdc77bb9afa41cc4b2255261839de99fcec1
appishiwata <app.ishiwata@gmail.com>
2023-07-09 17:03:22 +0900

    githubcopilotとunityのデバッグ機能を試す

--- Assets/Scenes/Sample0709/SampleCanvas.cs
+++ Assets/Scenes/Sample0709/SampleCanvas.cs
@@ -1,14 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
 public class SampleCanvas : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _titleText;
-    
-    void Start()
+
+    async void Start()
     {   
         _titleText.text = "Hello World";
+        // UniTaskで１秒待つ
+        await UniTask.Delay(1000);
+        _titleText.text = "Hello World2";
     }
 }
