b9b1e215071e2ee12ba787a8ebbdb791ae1f13f3
appishiwata <app.ishiwata@gmail.com>
2023-07-13 05:46:48 +0900

    ContextMenuの追加

--- Assets/Scenes/Particle/ParticleSample.cs
+++ Assets/Scenes/Particle/ParticleSample.cs
@@ -9,4 +9,19 @@ public class ParticleSample : MonoBehaviour
         // パーティクルを真ん中に表示する
         _particleSystem.transform.position = Vector3.zero;
     }
+
+    #if UNITY_EDITOR
+    // ContextMenuを使ってメニューを追加する
+    [ContextMenu("Play")]
+    void Play()
+    {
+        _particleSystem.Play();
+    }
+
+    [ContextMenu("Stop")]
+    void Stop()
+    {
+        _particleSystem.Stop();
+    }
+    #endif
 }
