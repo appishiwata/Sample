eb14eb0c2c9c7211ab1f67709d63325216df7322
appishiwata <app.ishiwata@gmail.com>
2023-07-14 06:55:07 +0900

    カードが拡散するアニメーション ※拡散する位置を先に_positionsに保持しておく必要がある

--- Assets/Scenes/Clone/CloneSample.cs
+++ Assets/Scenes/Clone/CloneSample.cs
@@ -1,28 +1,32 @@
+using System.Linq;
+using DG.Tweening;
 using UnityEngine;
 
 public class CloneSample : MonoBehaviour
 {
     [SerializeField] GameObject _prefab;
     [SerializeField] Transform _parent;
+    [SerializeField] Transform[] _positions;
 
     private void Start()
     {
-        // 1個だけ生成する
-        //Instantiate(_prefab, _parent);
-
-        // 3個生成する
-        for (int i = 0; i < 3; i++)
+        // _positionsの位置を確認する あらかじめ入れておく
+        _positions.Select(x => x.position).ToList().ForEach(x => Debug.Log(x));
+        
+        // 5個生成する
+        for (int i = 0; i < 5; i++)
         {
-            // 生成したオブジェクトを変数に入れる
             GameObject obj = Instantiate(_prefab, _parent);
-            // 生成したオブジェクトのCardコンポーネントを取得する
             Card card = obj.GetComponent<Card>();
-            // CardコンポーネントのSetCardDataメソッドを呼び出す
             card.SetCardData(new CardData()
             {
                 icon = Resources.Load<Sprite>("FaceIcons/boy_0" + (i+1)),
                 name = "Card" + (i+1)
             });
+            // 生成したオブジェクトを_positionsの位置に移動させる
+            obj.transform.DOMove(_positions[i].transform.position, 1f)
+                .SetEase(Ease.OutBack)
+                .SetDelay(0.5f);
         }
     }
 }
