26ae20ab516ad900b1253c91d874c1575397ae9f
appishiwata <app.ishiwata@gmail.com>
2023-07-15 09:28:19 +0900

    Cubeのサイズをスライダーで調整出来るように

--- Assets/Scenes/UITips/UITipsSample.cs
+++ Assets/Scenes/UITips/UITipsSample.cs
@@ -6,14 +6,31 @@ using UnityEngine.UI;
 public class UITipsSample : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _titleText;
-    [SerializeField] Slider _slider;
+    [SerializeField] Slider _hpSlider;
+    
+    [SerializeField] Slider _cubeSizeSlider;
+    [SerializeField] GameObject _cube;
     
     void Start()
     {
         _titleText.text = "UITips";
         // _sliderの値を0.5にする
-        //_slider.value = 0.5f;
+        //_hpSlider.value = 0.5f;
         // _sliderの値をdotweenで1秒かけて1にする
-        _slider.DOValue(1f, 2f);
+        _hpSlider.DOValue(1f, 2f);
+        
+        // _cubeSizeSliderの値を0.5にする
+        _cubeSizeSlider.value = 0.5f;
+        
+        // _cubeのscaleの初期値を_sliderの値にする
+        _cube.transform.localScale = Vector3.one * _cubeSizeSlider.value;
+        
+        // _cubeSizeSliderの値と_cubeのScaleを連動させる
+        _cubeSizeSlider.onValueChanged.AddListener(OnCubeSizeSliderValueChanged);
+    }
+
+    private void OnCubeSizeSliderValueChanged(float arg0)
+    {
+        _cube.transform.localScale = Vector3.one * arg0;
     }
 }
