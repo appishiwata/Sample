d5c90f973730e505adaffa5b1f21cb2346e17783
appishiwata <app.ishiwata@gmail.com>
2023-07-15 09:06:22 +0900

    Sliderを追加してValue変更のアニメーション

--- Assets/Scenes/UITips/UITipsSample.cs
+++ Assets/Scenes/UITips/UITipsSample.cs
@@ -1,12 +1,19 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UITipsSample : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _titleText;
+    [SerializeField] Slider _slider;
     
     void Start()
     {
         _titleText.text = "UITips";
+        // _sliderの値を0.5にする
+        //_slider.value = 0.5f;
+        // _sliderの値をdotweenで1秒かけて1にする
+        _slider.DOValue(1f, 2f);
     }
 }
