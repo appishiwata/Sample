bed93fc29a8e36e7901f351370021ebf2d5f2d80
appishiwata <app.ishiwata@gmail.com>
2023-07-19 03:02:14 +0900

    シェーダーを作ってマテリアルに設定、そのマテリアルをキューブに設定

--- Assets/Scenes/3DModel/ModelSample.cs
+++ Assets/Scenes/3DModel/ModelSample.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ModelSample : MonoBehaviour
 {
+    [SerializeField] GameObject _floorModel;
+    
     void Start()
     {
-        
+        _floorModel.transform.position = new Vector3(0, 0, 0);
     }
 }
