b450780abc7d4585f740aea4e1825d1e89ff6281
appishiwata <app.ishiwata@gmail.com>
2023-07-21 05:35:37 +0900

    円形画像のfill amountを試す

--- Assets/Scenes/UITips/ImageFillSample.cs
+++ Assets/Scenes/UITips/ImageFillSample.cs
@@ -1,12 +1,19 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ImageFillSample : MonoBehaviour
 {
     [SerializeField] Image _hpFillImage;
-
+    [SerializeField] Image _circleFillImage;
+    
     void Start()
     {
         _hpFillImage.fillAmount = 0.5f;
+        _circleFillImage.fillAmount = 0.5f;
+        
+        // 0.5秒後にHPを1にする DOTWeenを使う
+        _hpFillImage.DOFillAmount(1f, 0.5f).SetDelay(0.5f);
+        _circleFillImage.DOFillAmount(1f, 0.5f).SetDelay(0.5f);
     }
 }
