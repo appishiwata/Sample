39495e382789e2098b95672910f35dd6cd803741
appishiwata <app.ishiwata@gmail.com>
2023-07-22 06:23:40 +0900

    Cardをクリックした時にシーン側のスクリプトで処理を待ち受けられるように Subject + next + Subscribe

--- Assets/Scenes/Clone/Card.cs
+++ Assets/Scenes/Clone/Card.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,22 @@ public class Card : MonoBehaviour
 {
     [SerializeField] Image _icon;
     [SerializeField] TextMeshProUGUI _name;
+    [SerializeField] Button _selectButton;
+    
+    public static Subject<CardData> OnClickedSelectButton = new();
+    
+    public CardData CardData { get; private set; }
+    
+    void Start()
+    {
+        _selectButton.OnClickAsObservable()
+            .Subscribe(_ => OnClickedSelectButton.OnNext(CardData));
+    }
     
     public void SetCardData(CardData cardData)
     {
+        CardData = cardData;
+        
         _icon.sprite = cardData.icon;
         _name.text = cardData.name;
     }
--- Assets/Scenes/Clone/CloneSample.cs
+++ Assets/Scenes/Clone/CloneSample.cs
@@ -1,18 +1,21 @@
 using System.Linq;
 using DG.Tweening;
+using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CloneSample : MonoBehaviour
 {
     [SerializeField] GameObject _prefab;
     [SerializeField] Transform _parent;
     [SerializeField] Transform[] _positions;
+    [SerializeField] Button _cloneButton;
 
-    private void Start()
+    private async void Start()
     {
         // _positionsの位置を確認する あらかじめ入れておく
         _positions.Select(x => x.position).ToList().ForEach(x => Debug.Log(x));
-        
+
         // 5個生成する
         for (int i = 0; i < 10; i++)
         {
@@ -27,6 +30,17 @@ public class CloneSample : MonoBehaviour
             obj.transform.DOMove(_positions[i].transform.position, 1f)
                 .SetEase(Ease.OutBack)
                 .SetDelay(0.5f);
+        }
+
+        // _closeButtonが押されたらこのusingブロックを抜ける
+        using (Card.OnClickedSelectButton.Subscribe(data =>
+        {
+            Debug.Log(data.name);
+        }))
+        {
+            // _closeButtonが押されるまで待機する
+            await _cloneButton.OnClickAsObservable().First();
+            Debug.Log("CloneButton Clicked");
         }
     }
 }
