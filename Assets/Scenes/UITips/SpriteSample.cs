944ec6576d45fef74c6e2de7510dbd1a37f6ece1
appishiwata <app.ishiwata@gmail.com>
2023-07-23 03:55:55 +0900

    Sprite 反転や移動

--- Assets/Scenes/UITips/SpriteSample.cs
+++ Assets/Scenes/UITips/SpriteSample.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class SpriteSample : MonoBehaviour
@@ -7,5 +8,13 @@ public class SpriteSample : MonoBehaviour
     void Start()
     {
         _spriteRenderer.color = Color.gray;
+        
+        // 反転させる
+        _spriteRenderer.flipX = true;
+        
+        // x5.5からx-5.5に移動させる
+        _spriteRenderer.transform.position = new Vector3(5.5f, 0f, 0f);
+        _spriteRenderer.transform.DOMoveX(-5.5f, 3f)
+            .SetDelay(0.5f);
     }
 }
