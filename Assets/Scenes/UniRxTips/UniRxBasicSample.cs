2047634e52dca442345bae1d80192320e476bdd0
appishiwata <app.ishiwata@gmail.com>
2023-07-25 06:20:46 +0900

    オペレータの使用 Select,Where,Take

--- Assets/Scenes/UniRxTips/UniRxBasicSample.cs
+++ Assets/Scenes/UniRxTips/UniRxBasicSample.cs
@@ -27,5 +27,30 @@ public class UniRxBasicSample : MonoBehaviour
         subject2.OnNext("Hello World3");
         subject2.OnError(new Exception("Error!"));
         subject2.OnNext("Hello World4");
+        
+        // オペレータの使用
+        // Selectでイベントを加工する
+        var subject3 = new Subject<string>();
+        subject3.Select(x => x + "!")
+            .Subscribe(x => Debug.Log(x));
+        subject3.OnNext("Hello World");
+        
+        // Whereでイベントをフィルタリングする
+        var subject4 = new Subject<int>();
+        subject4.Where(x => x % 2 == 0)
+            .Subscribe(x => Debug.Log(x));
+        subject4.OnNext(1);
+        subject4.OnNext(2); // これだけ表示される
+        subject4.OnNext(3);
+        
+        // Takeでイベントを指定した数だけ取得する
+        var subject5 = new Subject<int>();
+        subject5.Take(3)
+            .Subscribe(x => Debug.Log(x));
+        subject5.OnNext(1);
+        subject5.OnNext(2);
+        subject5.OnNext(3); // ここまで表示される
+        subject5.OnNext(4);
+        subject5.OnNext(5);
     }
 }
