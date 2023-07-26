e49b3aefdfa921ffe5ea0cd2d104720e050d549e
appishiwata <app.ishiwata@gmail.com>
2023-07-27 08:46:46 +0900

    Addressableの導入

--- Assets/Scenes/ScriptTips/ScriptTipsSample.cs
+++ Assets/Scenes/ScriptTips/ScriptTipsSample.cs
@@ -1,14 +1,24 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.UI;
 
 public class ScriptTipsSample : MonoBehaviour
 {
-    [SerializeField] Image _image;
+    [SerializeField] Image _image1;
+    [SerializeField] Image _image2;
+
     void Start()
     {
         // LoadAssetAtPathのサンプル
         var buttonSprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Textures/UI/button_square.png");
-        _image.sprite = buttonSprite;
+        _image1.sprite = buttonSprite;
+        
+        // Addressableのサンプル
+        Addressables.LoadAssetAsync<Sprite>("Assets/Textures/UI/button_square.png")
+            .Completed += handle =>
+            {
+                _image2.sprite = handle.Result;
+            };
     }
 }
