e9cfbb87044e8b891308025e0c20ea936b32776f
appishiwata <app.ishiwata@gmail.com>
2023-08-01 00:26:03 +0900

    名前空間を使ってみる

--- Assets/Scenes/Standard/StanDardSample.cs
+++ Assets/Scenes/Standard/StanDardSample.cs
@@ -2,25 +2,28 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class StanDardSample : MonoBehaviour
+namespace Scenes.Standard
 {
-    void Start()
+    public class StandardSample : MonoBehaviour
     {
-        // ランダムな値をDebug.Logで表示する
-        Debug.Log(Random.Range(0, 100));
+        void Start()
+        {
+            // ランダムな値をDebug.Logで表示する
+            Debug.Log(Random.Range(0, 100));
         
-        // 色をつけてDebug.Logで表示する
-        Debug.Log("<color=red>赤色</color>");
+            // 色をつけてDebug.Logで表示する
+            Debug.Log("<color=red>赤色</color>");
         
-        // 検索しやすいDebug.Logで表示する
-        Debug.Log("【エラー】");
+            // 検索しやすいDebug.Logで表示する
+            Debug.Log("【エラー】");
         
-        // 配列定義 > リスト変換 > ForEachで表示
-        string[] list = {"A", "B", "C"};
-        list.ToList().ForEach(Debug.Log);
+            // 配列定義 > リスト変換 > ForEachで表示
+            string[] list = {"A", "B", "C"};
+            list.ToList().ForEach(Debug.Log);
         
-        // リスト定義 > ForEachで表示
-        List<string> list2 = new List<string>() {"A", "B", "C"};
-        list2.ForEach(Debug.Log);
+            // リスト定義 > ForEachで表示
+            List<string> list2 = new List<string>() {"A", "B", "C"};
+            list2.ForEach(Debug.Log);
+        }
     }
 }
