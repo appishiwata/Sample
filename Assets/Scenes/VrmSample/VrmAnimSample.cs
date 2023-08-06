a358710d59352f24ab656069a305203cedc82d6d
appishiwata <app.ishiwata@gmail.com>
2023-08-07 04:31:26 +0900

    Enterで表情操作

--- Assets/Scenes/VrmSample/VrmAnimSample.cs
+++ Assets/Scenes/VrmSample/VrmAnimSample.cs
@@ -25,5 +25,14 @@ public class VrmAnimSample : MonoBehaviour
             {
                 _animator.Play("Layer_look_away", 1, 0.0f);
             });
+        
+        // UniRXでEnterキーを押したらBlendShapeを変更する
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.Return))
+            .Subscribe(_ =>
+            {
+                _skinnedMeshRenderer.SetBlendShapeWeight(145, 100f);
+                _skinnedMeshRenderer.SetBlendShapeWeight(149, 100f);
+            });
     }
 }
