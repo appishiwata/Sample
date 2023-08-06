11e924654cd3ec916f74a2253ee89a5aaf7d5247
appishiwata <app.ishiwata@gmail.com>
2023-08-07 00:22:25 +0900

    UniRXでスペースを待機するように

--- Assets/Scenes/VrmSample/VrmAnimSample.cs
+++ Assets/Scenes/VrmSample/VrmAnimSample.cs
@@ -1,16 +1,20 @@
-using Cysharp.Threading.Tasks;
+using UniRx;
 using UnityEngine;
 
 public class VrmAnimSample : MonoBehaviour
 {
     [SerializeField] Animator _animator;
 
-    async void Start()
+    void Start()
     {
         _animator.SetInteger("animBaseInt", 3);
         
-        // UniTaskでスペースキーを押したらアニメーションを変更する
-        await UniTask.WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
-        _animator.Play("Layer_look_away", 1, 0.0f);
+        // UniRXでスペースキーを押したらアニメーションを変更する
+        Observable.EveryUpdate()
+            .Where(_ => Input.GetKeyDown(KeyCode.Space))
+            .Subscribe(_ =>
+            {
+                _animator.Play("Layer_look_away", 1, 0.0f);
+            });
     }
 }
