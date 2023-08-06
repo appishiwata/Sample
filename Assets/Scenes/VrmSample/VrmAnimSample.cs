9682fcbbcd68c5d6c93d46fb42788a66c8cb12d1
appishiwata <app.ishiwata@gmail.com>
2023-08-07 00:16:18 +0900

    UniTaskでスペース押されるまで待機 押されたらアニメーション変更

--- Assets/Scenes/VrmSample/VrmAnimSample.cs
+++ Assets/Scenes/VrmSample/VrmAnimSample.cs
@@ -1,11 +1,16 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class VrmAnimSample : MonoBehaviour
 {
     [SerializeField] Animator _animator;
-    
-    void Start()
+
+    async void Start()
     {
         _animator.SetInteger("animBaseInt", 3);
+        
+        // UniTaskでスペースキーを押したらアニメーションを変更する
+        await UniTask.WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        _animator.Play("Layer_look_away", 1, 0.0f);
     }
 }
