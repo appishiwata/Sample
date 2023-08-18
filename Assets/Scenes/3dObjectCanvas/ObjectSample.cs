c3f6c750511c7642b5162b29e466ce4582dd5064
appishiwata <app.ishiwata@gmail.com>
2023-08-18 23:58:25 +0900

    子供のレイヤーも変更

--- Assets/Scenes/3dObjectCanvas/ObjectSample.cs
+++ Assets/Scenes/3dObjectCanvas/ObjectSample.cs
@@ -6,8 +6,17 @@ public class ObjectSample : MonoBehaviour
 {
     [SerializeField] GameObject _object;
     
+    [SerializeField] GameObject _objectParentChild;
+    
     void Start()
     {
         _object.layer = 1;
+        
+        // 子供のレイヤーも変更
+        _objectParentChild.layer = 1;
+        foreach (Transform child in _objectParentChild.transform)
+        {
+            child.gameObject.layer = 1;
+        }
     }
 }
