b1784fc26417d81ab71e0196f6d4e50bcbad1325
appishiwata <app.ishiwata@gmail.com>
2023-08-19 23:55:54 +0900

    layerの変更確認

--- Assets/Scenes/3dObjectCanvas/ObjectSample.cs
+++ Assets/Scenes/3dObjectCanvas/ObjectSample.cs
@@ -10,10 +10,13 @@ public class ObjectSample : MonoBehaviour
     
     void Start()
     {
+
         _object.layer = 1;
         
         // 子供のレイヤーも変更
+        Debug.Log(_objectParentChild.layer);
         _objectParentChild.layer = 1;
+        Debug.Log(_objectParentChild.layer);
         foreach (Transform child in _objectParentChild.transform)
         {
             child.gameObject.layer = 1;
