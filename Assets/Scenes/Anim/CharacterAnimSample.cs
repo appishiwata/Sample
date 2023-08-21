d642a0157116c155f85359a4e43bb7348a0af2ae
appishiwata <app.ishiwata@gmail.com>
2023-08-21 23:56:12 +0900

    2Dアニメ用サンプル作成

---
+++ Assets/Scenes/Anim/CharacterAnimSample.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterAnimSample : MonoBehaviour
+{
+    [SerializeField] Image _image;
+    [SerializeField] Sprite[] _sprites;
+    [SerializeField] Button _smileButton;
+
+    async void Start()
+    {
+        // DOTweenでスケールを繰り返し変更
+        _image.transform.DOScale(0.61f, 1.5f)
+            .SetLoops(-1, LoopType.Yoyo);
+        
+        // Unitaskでボタンを押すまで待つ
+        await _smileButton.OnClickAsync();
+        _image.sprite = _sprites[3];
+    }
+}
